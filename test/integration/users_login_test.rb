0762e27667450b9ad6e10636f3f2d264c97154d9
ykinoshita <ykinoshita@10.64.26.34>
2018-11-07 19:11:26 +0900

    9.4まで完了（試験は動かしてない）

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-  
+
   test "login with invalid information" do
-    
+
     get login_path
     assert_template 'sessions/new'
     post login_path, params: { session: { email: "", password: "" } }
@@ -12,5 +12,19 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get root_path
     assert flash.empty?
   end
-  
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not_empty cookies['remember_token']
+  end
+
+  test "login without remembering" do
+    # クッキーを保存してログイン
+    log_in_as(@user, remember_me: '1')
+    delete logout_path
+    # クッキーを削除してログイン
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies['remember_token']
+  end
+
 end
